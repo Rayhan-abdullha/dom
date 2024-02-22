0888b0e331632f71e8d6383eb1d7855533e25b75
Rayhan-abdullha <captainrayhan215@gmail.com>
2024-02-22 11:37:30 +0600

    dom practice

--- demo/main.js
+++ demo/main.js
@@ -1,6 +1,26 @@
 // console.log(window);
 // window.alert('coder')
 
+// console.log(document.getElementById("div-id").innerHTML);
+// console.log(document.getElementById("div-id").innerText);
+// console.log(document.getElementById("div-id").textContent);
+
 // window.addEventListener("resize", () => {
 //   console.log(window.innerWidth);
 // });
+
+const divWidth = document.getElementById("div-id");
+divWidth.style.color = "red";
+
+// html collections
+let listItem = document.getElementsByClassName("list-item");
+
+// convert html collection to array
+listItem = Array.from(listItem);
+listItem.forEach((item) => {
+  item.style.color = "green";
+});
+
+let listItem1 = document.querySelectorAll(".list-item1");
+
+listItem1.forEach((item) => (item.style.color = "tomato"));
